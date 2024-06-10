7a64af12c63cf9a147c305ea121a3b50fee8560a
Kartik Sanil <22kartiksanil@gmail.com>
2024-06-10 09:07:14 +0530

    Update hello_world.js

--- Practice/hello_world.js
+++ Practice/hello_world.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+console.log('Hello World');
+console.log('hello' + ' there' + ', hiya');
+var petDog = 'Rex';
+var petCat = 'Pepper';
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+var catSound = 'purr';
+var dogSound = 'woof';
+console.log(petDog + ' says ' + dogSound);
+console.log(petCat + ' says ' + catSound);
+catSound = 'meow';
+console.log(petCat + ' now says ' + catSound);
