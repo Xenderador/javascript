9b8df17f83b49222f8cfa685f7cd330240ca6255
Kartik Sanil <22kartiksanil@gmail.com>
2024-06-21 11:28:31 +0400

    functional programming

---
+++ Practice/recursion.js
@@ -0,0 +1,21 @@
+// simple printing done here
+function example() {
+	console.log('line one');
+	console.log('line two');
+	console.log('line three');
+}
+example();
+// using recursion here
+let counter = 3;
+function ex() {
+	console.log(counter);
+	counter--;
+	if (counter === 0) return;
+	ex();
+	/* if (counter == 0) {
+		return;
+	} else {
+		ex();
+	} */
+}
+ex();
